b26aa3f2f311adb6ad1a3acef327bda347cfb670
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-11-08 15:38:45 +0100

    refactor: remove auth routers

--- api/v1/routes/main.py
+++ api/v1/routes/main.py
@@ -5,5 +5,5 @@ from api.v1.routes.image import image_router
 
 main_router = APIRouter(prefix="/api/v1")
 
-main_router.include_router(router=auth)
+# main_router.include_router(router=auth)
 main_router.include_router(router=image_router)
